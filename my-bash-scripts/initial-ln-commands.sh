94c57c23c94b83c19cef331d9a4fa1c1f86ba493
pesarkhobeee <ahmadian.farid.1988@gmail.com>
2025-12-16 14:44:13 +0100

    add pre-commit for yaml check

--- my-bash-scripts/initial-ln-commands.sh
+++ my-bash-scripts/initial-ln-commands.sh
@@ -2,3 +2,6 @@ ln -s ~/.config/vimrc ~/.vimrc
 ln -s ~/.config/zshrc ~/.zshrc
 ln -s ~/.config/wezterm.lua ~/.wezterm.lua
 ln -s ~/.config/gitconfig ~/.gitconfig
+mkdir -p ~/.git-hooks
+ln -s ~/.config/my-bash-scripts/pre-commit ~/.git-hooks/pre-commit
+git config --global core.hooksPath ~/.git-hooks
--- my-bash-scripts/my-k8s-schema-checker.sh
+++ my-bash-scripts/my-k8s-schema-checker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to validate Kubernetes YAML files against multiple schema locations using kubeconform.
-kubeconform -summary -kubernetes-version 1.33.5 -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' "$1"
+kubeconform -strict -summary -kubernetes-version 1.33.5 -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' "$1"
